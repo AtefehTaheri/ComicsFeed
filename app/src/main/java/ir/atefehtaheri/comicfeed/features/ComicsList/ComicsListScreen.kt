da3fc6071e5dcdb4b6c10cf91cc668e0cf9c1fa2
AtefehTaheri <e.atefehtaheri@gmail.com>
2024-05-22 14:05:10 +0330

    Feature3 : Add search feature and sort comics

--- app/src/main/java/ir/atefehtaheri/comicfeed/features/ComicsList/ComicsListScreen.kt
+++ app/src/main/java/ir/atefehtaheri/comicfeed/features/ComicsList/ComicsListScreen.kt
@@ -4,18 +4,28 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ir.atefehtaheri.comicfeed.data.ComicsList.remote.model.ComicItem
@@ -29,20 +39,26 @@ internal fun ComicsListRoute(
 ) {
 
     val uiState by comicsViewModel.uiState.collectAsState()
+    val searchText by comicsViewModel.searchText.collectAsState()
+
 
     ComicsListScreen(
-        uiState = uiState
+        uiState = uiState,
+        onQueryChange = comicsViewModel::onSearchTextChange,
+        searchText = searchText,
     )
 }
 
 @Composable
 private fun ComicsListScreen(
     uiState: ComicsUiState,
+    onQueryChange: (String) -> Unit,
+    searchText: String,
 ) {
         if (uiState.isLoading) {
             LoadingState()
         } else {
-            ShowListState(uiState.comicsList)
+            ShowListState(uiState.comicsList,onQueryChange,searchText)
         }
 }
 
@@ -65,20 +81,49 @@ private fun LoadingState(modifier: Modifier = Modifier) {
 @Composable
 private fun ShowListState(
     comicslist: List<ComicItem>,
+    onQueryChange: (String) -> Unit,
+    searchText: String,
     modifier: Modifier = Modifier,
 ) {
     val listState = rememberLazyListState()
+    val softwareKeyboardController = LocalSoftwareKeyboardController.current
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-
-    LazyColumn(modifier = modifier.padding(16.dp), state = listState) {
-        items(comicslist){
-            ComicListItem(comicItem = it)
+        TextField(
+            value = searchText,
+            onValueChange = onQueryChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp, 15.dp),
+            shape = RoundedCornerShape(25.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            textStyle = MaterialTheme.typography.titleSmall,
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    softwareKeyboardController?.hide()
+                }
+            ),
+            singleLine = true,
+            trailingIcon = {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "",
+                )
+            },
+            placeholder = { Text("Search Text") }
+        )
+        LazyColumn(modifier = modifier.padding(16.dp), state = listState) {
+            items(comicslist) {
+                ComicListItem(comicItem = it)
+            }
         }
     }
-}}
+}
 
 
--- app/src/main/java/ir/atefehtaheri/comicfeed/features/ComicsList/ComicsViewModel.kt
+++ app/src/main/java/ir/atefehtaheri/comicfeed/features/ComicsList/ComicsViewModel.kt
@@ -6,7 +6,11 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.atefehtaheri.comicfeed.data.ComicsList.repository.ComicRepository
 import ir.atefehtaheri.comicfeed.features.ComicsList.UiState.ComicsUiState
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,8 +22,25 @@ class ComicsViewModel @Inject constructor(
 ) : ViewModel() {
 
 
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
     private var _uiState = MutableStateFlow<ComicsUiState>(ComicsUiState())
-    val uiState: StateFlow<ComicsUiState> = _uiState
+    val uiState: StateFlow<ComicsUiState> =  searchText
+        .combine(_uiState) { searchText, comicsUiState ->
+            if (searchText.isBlank()) {
+                comicsUiState
+            }
+            val filteredList= comicsUiState.comicsList.filter {
+                it.title.uppercase().contains(searchText.trim().uppercase()) ||
+                        it.transcript.uppercase().contains(searchText.trim().uppercase())
+            }.sortedWith (compareBy({ it.year }, { it.month },{ it.day }))
+            comicsUiState.copy(comicsList = filteredList)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = _uiState.value
+        )
 
         init {
              loadComicList()
@@ -34,7 +55,9 @@ class ComicsViewModel @Inject constructor(
                 }
             }
         }
-
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
 
 
 }
